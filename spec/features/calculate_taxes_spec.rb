59654aae14c89c31d2fc56d6d3008a08f6a4c705
rcoan <rvc.coan@gmail.com>
2021-10-21 19:25:45 -0300

    add the expectation for all behavior tests

--- spec/features/calculate_taxes_spec.rb
+++ spec/features/calculate_taxes_spec.rb
@@ -15,62 +15,105 @@ RSpec.describe 'Calculate taxes' do
       ].to_json
     end
 
+    let(expected_response) do
+      [{"tax":0},{"tax":0}].to_json
+    end
+
     it 'returns all taxes as zero' do
-      expect(subject).to  eq([{"tax":0},{"tax":0},{"tax":0}].to_json)
+      expect(subject).to  eq(expected_response)
     end
   end
 
   context 'Second input exemple' do
     let(:input) do
       [
-        {"operation":"buy", "unit-cost": 10, "quantity": 10000},
-        {"operation":"sell", "unit-cost":20, "quantity": 5000},
-        {"operation":"sell", "unit-cost":5, "quantity":5000 }
+        {"operation":"buy", "unit-cost": 10, "quantity": 10_000},
+        {"operation":"sell", "unit-cost":20, "quantity": 5_000},
+        {"operation":"sell", "unit-cost":5, "quantity":5_000 }
       ].to_json
     end
 
+    let(expected_response) do
+      [{"tax":10_000},{"tax":0}].to_json
+    end
+
+    it 'returns the first tax as 10_000 and the secon zero' do
+      expect(subject).to  eq(expected_response)
+    end
   end
 
   context 'Third input exemple' do
     let(:input) do
       [
-        {"operation":"buy", "unit-cost": 10, "quantity": 10000},
-        {"operation":"sell", "unit-cost":5, "quantity": 5000},
-        {"operation":"sell", "unit-cost":20, "quantity":5000 }
+        {"operation":"buy", "unit-cost": 10, "quantity": 10_000},
+        {"operation":"sell", "unit-cost":5, "quantity": 5_000},
+        {"operation":"sell", "unit-cost":20, "quantity":5_000 }
       ].to_json
+    end
+
+    let(expected_response) do
+      [{"tax":0},{"tax":5_000}].to_json
+    end
+
+    it 'returns the first tax as zero and the second as 5_000' do
+      expect(subject).to  eq(expected_response)
     end
   end
 
   context 'Fourth input exemple' do
     let(:input) do
       [
-        {"operation":"buy", "unit-cost": 10, "quantity": 10000},
-        {"operation":"buy", "unit-cost":25, "quantity": 5000},
-        {"operation":"sell", "unit-cost":15, "quantity":10000 }
+        {"operation":"buy", "unit-cost": 10, "quantity": 10_000},
+        {"operation":"buy", "unit-cost":25, "quantity": 5_000},
+        {"operation":"sell", "unit-cost":15, "quantity":10_000 }
       ].to_json
+    end
+
+    let(expected_response) do
+      [{"tax":0}].to_json
+    end
+
+    it 'returns the only sell tax as zero' do
+      expect(subject).to  eq(expected_response)
     end
   end
 
   context 'Fifth input exemple' do
     let(:input) do
       [
-        {"operation":"buy", "unit-cost": 10, "quantity": 10000},
-        {"operation":"buy", "unit-cost":25, "quantity": 5000},
-        {"operation":"sell", "unit-cost":15, "quantity":10000 },
-        {"operation":"sell", "unit-cost":25, "quantity": 5000}
+        {"operation":"buy", "unit-cost": 10, "quantity": 10_000},
+        {"operation":"buy", "unit-cost":25, "quantity": 5_000},
+        {"operation":"sell", "unit-cost":15, "quantity":10_000 },
+        {"operation":"sell", "unit-cost":25, "quantity": 5_000}
       ].to_json
+    end
+
+    let(expected_response) do
+      [{"tax":0},{"tax":10_000}].to_json
+    end
+
+    it 'returns the first tax as zero and the second as 10_000' do
+      expect(subject).to  eq(expected_response)
     end
   end
 
   context 'Sixth input exemple' do
     let(:input) do
       [
-        {"operation":"buy", "unit-cost": 10, "quantity": 10000},
-        {"operation":"sell", "unit-cost":2, "quantity": 5000},
-        {"operation":"sell", "unit-cost":20, "quantity":2000 },
-        {"operation":"sell", "unit-cost":20, "quantity": 2000},
-        {"operation":"sell", "unit-cost":25, "quantity": 1000}
+        {"operation":"buy", "unit-cost": 10, "quantity": 10_000},
+        {"operation":"sell", "unit-cost":2, "quantity": 5_000},
+        {"operation":"sell", "unit-cost":20, "quantity":2_000 },
+        {"operation":"sell", "unit-cost":20, "quantity": 2_000},
+        {"operation":"sell", "unit-cost":25, "quantity": 1_000}
       ].to_json
+    end
+
+    let(expected_response) do
+      [{"tax":0},{"tax":0},{"tax":0},{"tax":3_000}].to_json
+    end
+
+    it 'returns only the last tax as 3_000 and the rest zero' do
+      expect(subject).to  eq(expected_response)
     end
   end
 end
