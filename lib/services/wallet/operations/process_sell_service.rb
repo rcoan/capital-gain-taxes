0fefc9876e38fe8c0b9063f2d5e7456e4397d2df
rcoan <rvc.coan@gmail.com>
2021-10-22 13:26:46 -0300

    add logic to process sell operation service

--- lib/services/wallet/operations/process_sell_service.rb
+++ lib/services/wallet/operations/process_sell_service.rb
@@ -1,6 +1,8 @@
 module Wallets
   module Operations
     class ProcessSellService
+      OPERATION_PROFIT_TAX_THRESHOLD = 20_000
+
       def self.call(**args)
         new.call(**args)
       end
@@ -9,10 +11,32 @@ module Wallets
         unit_cost:,
         quantity:,
         total_stocks:,
-        weighted_average:,
+        weighted_average_cost:,
         total_profit:
       )
-      {}
+
+        current_operation_total_value = unit_cost * quantity
+
+        current_operation_profit = (weighted_average_cost - unit_cost) * quantity
+
+        current_operation_profit_after_losses = total_profit.negative? ? current_operation_profit + total_profit : current_operation_profit
+
+        # transform rules in validators
+        should_tax =
+          current_operation_profit > OPERATION_PROFIT_TAX_THRESHOLD &&
+          unit_cost > weighted_average_cost &&
+          current_operation_profit_after_losses.positive?
+
+
+        operation_tax = should_tax ? current_operation_profit_after_losses * 0.2 : 0
+        new_total_stocks = total_stocks - quantity
+        new_total_profit = current_operation_profit_after_losses
+
+        {
+          operation_tax: operation_tax,
+          total_stocks: new_total_stocks,
+          total_profit: new_total_profit
+        }
       end
 
 
