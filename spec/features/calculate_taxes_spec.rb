353edb9c1abbdb66a522547799f57c14bfe8fa7e
rcoan <rvc.coan@gmail.com>
2021-10-22 14:44:12 -0300

    Fix rubocop-rspec warnings

--- spec/features/calculate_taxes_spec.rb
+++ spec/features/calculate_taxes_spec.rb
@@ -6,17 +6,17 @@ require 'json'
 RSpec.describe 'Calculate taxes' do
   subject { Entrypoint.call(input) }
 
-  context 'First input exemple' do
+  context 'with First input exemple' do
     let(:input) do
       [
-        { "operation": 'buy', "unit-cost": 10, "quantity": 100 },
-        { "operation": 'sell', "unit-cost": 15, "quantity": 50 },
-        { "operation": 'sell', "unit-cost": 15, "quantity": 50 }
+        { operation: 'buy', 'unit-cost': 10, quantity: 100 },
+        { operation: 'sell', 'unit-cost': 15, quantity: 50 },
+        { operation: 'sell', 'unit-cost': 15, quantity: 50 }
       ].to_json
     end
 
     let(:expected_response) do
-      [{ "tax": 0.0 }, { "tax": 0.0 }].to_json
+      [{ tax: 0.0 }, { tax: 0.0 }].to_json
     end
 
     it 'returns all taxes as zero' do
@@ -24,17 +24,17 @@ RSpec.describe 'Calculate taxes' do
     end
   end
 
-  context 'Second input exemple' do
+  context 'with Second input exemple' do
     let(:input) do
       [
-        { "operation": 'buy', "unit-cost": 10, "quantity": 10_000 },
-        { "operation": 'sell', "unit-cost": 20, "quantity": 5_000 },
-        { "operation": 'sell', "unit-cost": 5, "quantity": 5_000 }
+        { operation: 'buy', 'unit-cost': 10, quantity: 10_000 },
+        { operation: 'sell', 'unit-cost': 20, quantity: 5_000 },
+        { operation: 'sell', 'unit-cost': 5, quantity: 5_000 }
       ].to_json
     end
 
     let(:expected_response) do
-      [{ "tax": 10_000.0 }, { "tax": 0.0 }].to_json
+      [{ tax: 10_000.0 }, { tax: 0.0 }].to_json
     end
 
     it 'returns the first tax as 10_000 and the secon zero' do
@@ -42,17 +42,17 @@ RSpec.describe 'Calculate taxes' do
     end
   end
 
-  context 'Third input exemple' do
+  context 'with Third input exemple' do
     let(:input) do
       [
-        { "operation": 'buy', "unit-cost": 10, "quantity": 10_000 },
-        { "operation": 'sell', "unit-cost": 5, "quantity": 5_000 },
-        { "operation": 'sell', "unit-cost": 20, "quantity": 5_000 }
+        { operation: 'buy', 'unit-cost': 10, quantity: 10_000 },
+        { operation: 'sell', 'unit-cost': 5, quantity: 5_000 },
+        { operation: 'sell', 'unit-cost': 20, quantity: 5_000 }
       ].to_json
     end
 
     let(:expected_response) do
-      [{ "tax": 0.0 }, { "tax": 5_000.0 }].to_json
+      [{ tax: 0.0 }, { tax: 5_000.0 }].to_json
     end
 
     it 'returns the first tax as zero and the second as 5_000' do
@@ -60,17 +60,17 @@ RSpec.describe 'Calculate taxes' do
     end
   end
 
-  context 'Fourth input exemple' do
+  context 'with Fourth input exemple' do
     let(:input) do
       [
-        { "operation": 'buy', "unit-cost": 10, "quantity": 10_000 },
-        { "operation": 'buy', "unit-cost": 25, "quantity": 5_000 },
-        { "operation": 'sell', "unit-cost": 15, "quantity": 10_000 }
+        { operation: 'buy', 'unit-cost': 10, quantity: 10_000 },
+        { operation: 'buy', 'unit-cost': 25, quantity: 5_000 },
+        { operation: 'sell', 'unit-cost': 15, quantity: 10_000 }
       ].to_json
     end
 
     let(:expected_response) do
-      [{ "tax": 0.0 }].to_json
+      [{ tax: 0.0 }].to_json
     end
 
     it 'returns the only sell tax as zero' do
@@ -78,18 +78,18 @@ RSpec.describe 'Calculate taxes' do
     end
   end
 
-  context 'Fifth input exemple' do
+  context 'with Fifth input exemple' do
     let(:input) do
       [
-        { "operation": 'buy', "unit-cost": 10, "quantity": 10_000 },
-        { "operation": 'buy', "unit-cost": 25, "quantity": 5_000 },
-        { "operation": 'sell', "unit-cost": 15, "quantity": 10_000 },
-        { "operation": 'sell', "unit-cost": 25, "quantity": 5_000 }
+        { operation: 'buy', 'unit-cost': 10, quantity: 10_000 },
+        { operation: 'buy', 'unit-cost': 25, quantity: 5_000 },
+        { operation: 'sell', 'unit-cost': 15, quantity: 10_000 },
+        { operation: 'sell', 'unit-cost': 25, quantity: 5_000 }
       ].to_json
     end
 
     let(:expected_response) do
-      [{ "tax": 0.0 }, { "tax": 10_000.0 }].to_json
+      [{ tax: 0.0 }, { tax: 10_000.0 }].to_json
     end
 
     it 'returns the first tax as zero and the second as 10_000' do
@@ -97,19 +97,19 @@ RSpec.describe 'Calculate taxes' do
     end
   end
 
-  context 'Sixth input exemple' do
+  context 'with Sixth input exemple' do
     let(:input) do
       [
-        { "operation": 'buy', "unit-cost": 10, "quantity": 10_000 },
-        { "operation": 'sell', "unit-cost": 2, "quantity": 5_000 },
-        { "operation": 'sell', "unit-cost": 20, "quantity": 2_000 },
-        { "operation": 'sell', "unit-cost": 20, "quantity": 2_000 },
-        { "operation": 'sell', "unit-cost": 25, "quantity": 1_000 }
+        { operation: 'buy', 'unit-cost': 10, quantity: 10_000 },
+        { operation: 'sell', 'unit-cost': 2, quantity: 5_000 },
+        { operation: 'sell', 'unit-cost': 20, quantity: 2_000 },
+        { operation: 'sell', 'unit-cost': 20, quantity: 2_000 },
+        { operation: 'sell', 'unit-cost': 25, quantity: 1_000 }
       ].to_json
     end
 
     let(:expected_response) do
-      [{ "tax": 0.0 }, { "tax": 0.0 }, { "tax": 0.0 }, { "tax": 3_000.0 }].to_json
+      [{ tax: 0.0 }, { tax: 0.0 }, { tax: 0.0 }, { tax: 3_000.0 }].to_json
     end
 
     it 'returns only the last tax as 3_000 and the rest zero' do
