0cfa9f85d1b545cbb42ad2e15e29c66941b554c4
rcoan <rvc.coan@gmail.com>
2021-10-22 14:59:05 -0300

    improve proccess buy service structure

--- lib/services/wallets/operations/process_buy_service.rb
+++ lib/services/wallets/operations/process_buy_service.rb
@@ -12,15 +12,25 @@ module Wallets
                total_stocks:,
                weighted_average_cost:)
 
-        new_total_stocks = total_stocks + quantity
-        current_operation_total_value = unit_cost * quantity
-        total_expent_previously = total_stocks * weighted_average_cost
+        @new_total_stocks = total_stocks + quantity
+        @current_operation_total_value = unit_cost * quantity
+        @total_expended_previously = total_stocks * weighted_average_cost
 
-        new_weighted_average = (total_expent_previously + current_operation_total_value) / new_total_stocks
+        format_response(new_total_stocks, new_weighted_average)
+      end
 
+      private
+
+      def new_weighted_average
+        total_expended = @total_expended_previously + @current_operation_total_value
+
+        total_expended / @new_total_stocks
+      end
+
+      def format_response(total_stocks, weighted_average_cost)
         {
-          total_stocks: new_total_stocks,
-          weighted_average_cost: new_weighted_average
+          total_stocks: total_stocks,
+          weighted_average_cost: weighted_average_cost
         }
       end
     end
