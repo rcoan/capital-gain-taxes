b17ae1da4930ed3e659d3d8b9974bbc01c6afc07
rcoan <rvc.coan@gmail.com>
2021-10-22 09:06:45 -0300

    add logic to process buy operation

--- lib/models/wallet.rb
+++ lib/models/wallet.rb
@@ -1,10 +1,26 @@
 class Wallet
-  attr_reader :weighted_average, :total_stocks, :total_profit, :operations
+  attr_reader :weighted_average, :total_stocks, :total_profit, :operations, :taxes
 
   def initialize(operations:)
     @weighted_average = 0
     @total_profit = 0
     @total_stocks = 0
+    @taxes = []
     @operations = operations
+  end
+
+  def update(weighted_average: nil, total_stocks: nil)
+    @weighted_average = weighted_average unless weighted_average.nil?
+    @total_stocks = total_stocks unless total_stocks.nil?
+  end
+
+  def to_s
+    {
+      operations: @operations,
+      total_profit: @total_profit,
+      total_stocks: @total_stocks,
+      weighted_average: @weighted_average,
+      taxes: @taxes
+    }.to_s
   end
 end
--- lib/services/wallet/process_operations_tax_service.rb
+++ lib/services/wallet/process_operations_tax_service.rb
@@ -1,4 +1,33 @@
 module Wallets
   class ProcessOperationsTaxService
+    def self.call(*args)
+      new.call(*args)
+    end
+
+    def call(wallet)
+      wallet.operations.each do |operation|
+        operation_values = {
+          unit_cost: operation['unit-cost'],
+          quantity: operation['quantity'],
+          total_stocks: wallet.total_stocks,
+          weighted_average: wallet.total_profit
+        }
+
+        if operation['operation'] == 'buy'
+          result = Operations::ProcessBuyService.call(operation_values)
+          update_wallet(wallet, result)
+        elsif operation['operation'] == 'sell'
+        end
+      end
+      binding.pry
+
+      wallet
+    end
+
+    private
+
+    def update_wallet(wallet, new_attributes)
+      wallet.update(new_attributes)
+    end
   end
 end
