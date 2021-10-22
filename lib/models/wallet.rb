09893556908e549ffba0fca9c046fd1e2f233c7e
rcoan <rvc.coan@gmail.com>
2021-10-22 13:27:24 -0300

    add fields to store data from processed sells

--- lib/models/wallet.rb
+++ lib/models/wallet.rb
@@ -1,16 +1,23 @@
 class Wallet
-  attr_reader :weighted_average, :total_stocks, :total_profit, :operations, :taxes
+  attr_reader :weighted_average_cost, :total_stocks, :total_profit, :operations, :taxes
 
   def initialize(operations:)
-    @weighted_average = 0
+    @weighted_average_cost = 0
     @total_profit = 0
     @total_stocks = 0
     @taxes = []
     @operations = operations
   end
 
-  def update(weighted_average: nil, total_stocks: nil)
-    @weighted_average = weighted_average unless weighted_average.nil?
+  def update(
+    weighted_average_cost: nil,
+    total_stocks: nil,
+    operation_tax: nil,
+    total_profit: nil
+)
+    @weighted_average_cost = weighted_average_cost unless weighted_average_cost.nil?
     @total_stocks = total_stocks unless total_stocks.nil?
+    @total_profit = total_profit unless total_profit.nil?
+    @taxes.push(operation_tax) unless operation_tax.nil?
   end
 end
