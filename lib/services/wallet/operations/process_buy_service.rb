70c6be233106ef632c957b27dad87055ab2f9a4c
rcoan <rvc.coan@gmail.com>
2021-10-22 09:06:14 -0300

    add initial logic to process buy operation service

--- lib/services/wallet/operations/process_buy_service.rb
+++ lib/services/wallet/operations/process_buy_service.rb
@@ -1,6 +1,30 @@
 module Wallets
   module Operations
     class ProcessBuyService
+      def self.call(**args)
+        new.call(**args)
+      end
+
+      def call(
+        unit_cost:,
+        quantity:,
+        total_stocks:,
+        weighted_average:
+      )
+
+
+      new_total_stocks = total_stocks += quantity
+      current_operation_total_value = unit_cost * quantity
+      total_expent_previously = total_stocks * weighted_average
+
+      new_weighted_average = (total_expent_previously + current_operation_total_value)/new_total_stocks
+
+      {
+        total_stocks: new_total_stocks,
+        weighted_average: new_weighted_average
+      }
+      end
     end
   end
 end
+
