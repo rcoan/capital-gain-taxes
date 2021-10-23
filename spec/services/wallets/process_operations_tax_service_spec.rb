673fa5f63a24cc9d0b3f01f93aecb21d86faab89
rcoan <rvc.coan@gmail.com>
2021-10-23 13:00:43 -0300

    test the main behavior and implementation of proccess operations tax service

--- spec/services/wallets/process_operations_tax_service_spec.rb
+++ spec/services/wallets/process_operations_tax_service_spec.rb
@@ -4,5 +4,31 @@ require 'spec_helper'
 
 RSpec.describe Wallets::ProcessOperationsTaxService do
   describe '#call' do
+    it 'updates wallet after processing each operation and returns the same object' do
+      buy_result = {
+        operation_tax: 0,
+        total_stocks: 10,
+        weighted_average_cost: 10
+      }
+
+      sell_result = {
+        operation_tax: 0,
+        total_stocks: 0,
+        total_loss: 0
+      }
+
+      buy_operation = { 'operation' => 'buy', 'unit-cost' => 10, 'quantity' => 10 }
+      sell_operation = { 'operation' => 'sell', 'unit-cost' => 10, 'quantity' => 10 }
+      operations = [buy_operation, sell_operation]
+
+      wallet = Wallet.new(operations: operations)
+
+      allow(wallet).to receive(:update).and_call_original
+
+      expect(wallet).to receive(:update).with(buy_result).once
+      expect(wallet).to receive(:update).with(sell_result).once
+
+      expect(described_class.call(wallet)).to eq(wallet)
+    end
   end
 end
