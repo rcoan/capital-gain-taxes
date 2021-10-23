a73a96ff5926d5127b2f9adfc74e66aef405ba5d
rcoan <rvc.coan@gmail.com>
2021-10-22 21:08:43 -0300

    add tests and exceptions treatment to interactor

--- lib/interactors/process_tax_from_profit_interactor.rb
+++ lib/interactors/process_tax_from_profit_interactor.rb
@@ -6,13 +6,31 @@ class ProcessTaxFromProfitInteractor
   end
 
   def call(operations_json)
-    parsed_operations = JSON.parse(operations_json)
+    parsed_operations = parse_operations(operations_json)
     wallet = Wallet.new(operations: parsed_operations)
 
     wallet = Wallets::ProcessOperationsTaxService.call(wallet)
 
     WalletPresenter.new(wallet).json_formatted_taxes
+  rescue JSON::ParserError => e
+    return 'Invalid json string'
   rescue StandardError => e
     return e.message
+  end
+
+  private
+
+  def parse_operations(operations_json)
+    raise_invalid_input_error if operations_json.nil? || operations_json.empty?
+
+    parsed_operations = JSON.parse(operations_json)
+
+    raise_invalid_input_error if parsed_operations.nil? || parsed_operations.empty?
+
+    parsed_operations
+  end
+
+  def raise_invalid_input_error
+    raise StandardError.new('Operations cannot be null or a empty array')
   end
 end
--- spec/features/calculate_taxes_spec.rb
+++ spec/features/calculate_taxes_spec.rb
@@ -4,115 +4,150 @@ require 'spec_helper'
 
 RSpec.describe ProcessTaxFromProfitInteractor, 'Calculate taxes' do
   subject { ProcessTaxFromProfitInteractor.call(input) }
+  context 'With exemple cases from test' do
+    context 'with First input exemple' do
+      let(:input) do
+        [
+          { operation: 'buy', 'unit-cost': 10, quantity: 100 },
+          { operation: 'sell', 'unit-cost': 15, quantity: 50 },
+          { operation: 'sell', 'unit-cost': 15, quantity: 50 }
+        ].to_json
+      end
 
-  context 'with First input exemple' do
-    let(:input) do
-      [
-        { operation: 'buy', 'unit-cost': 10, quantity: 100 },
-        { operation: 'sell', 'unit-cost': 15, quantity: 50 },
-        { operation: 'sell', 'unit-cost': 15, quantity: 50 }
-      ].to_json
+      let(:expected_response) do
+        [{ tax: 0 }, { tax: 0 }, { tax: 0 }].to_json
+      end
+
+      it 'returns all taxes as zero' do
+        expect(subject).to  eq(expected_response)
+      end
     end
 
-    let(:expected_response) do
-      [{ tax: 0 }, { tax: 0 }, { tax: 0 }].to_json
+    context 'with Second input exemple' do
+      let(:input) do
+        [
+          { operation: 'buy', 'unit-cost': 10, quantity: 10_000 },
+          { operation: 'sell', 'unit-cost': 20, quantity: 5_000 },
+          { operation: 'sell', 'unit-cost': 5, quantity: 5_000 }
+        ].to_json
+      end
+
+      let(:expected_response) do
+        [{ tax: 0 }, { tax: 10_000.0 }, { tax: 0 }].to_json
+      end
+
+      it 'returns the first tax as 10_000 and the secon zero' do
+        expect(subject).to  eq(expected_response)
+      end
     end
 
-    it 'returns all taxes as zero' do
-      expect(subject).to  eq(expected_response)
+    context 'with Third input exemple' do
+      let(:input) do
+        [
+          { operation: 'buy', 'unit-cost': 10, quantity: 10_000 },
+          { operation: 'sell', 'unit-cost': 5, quantity: 5_000 },
+          { operation: 'sell', 'unit-cost': 20, quantity: 5_000 }
+        ].to_json
+      end
+
+      let(:expected_response) do
+        [{ tax: 0 }, { tax: 0 }, { tax: 5_000.0 }].to_json
+      end
+
+      it 'returns the first tax as zero and the second as 5_000' do
+        expect(subject).to  eq(expected_response)
+      end
+    end
+
+    context 'with Fourth input exemple' do
+      let(:input) do
+        [
+          { operation: 'buy', 'unit-cost': 10, quantity: 10_000 },
+          { operation: 'buy', 'unit-cost': 25, quantity: 5_000 },
+          { operation: 'sell', 'unit-cost': 15, quantity: 10_000 }
+        ].to_json
+      end
+
+      let(:expected_response) do
+        [{ tax: 0 }, { tax: 0 }, { tax: 0 }].to_json
+      end
+
+      it 'returns the only sell tax as zero' do
+        expect(subject).to  eq(expected_response)
+      end
+    end
+
+    context 'with Fifth input exemple' do
+      let(:input) do
+        [
+          { operation: 'buy', 'unit-cost': 10, quantity: 10_000 },
+          { operation: 'buy', 'unit-cost': 25, quantity: 5_000 },
+          { operation: 'sell', 'unit-cost': 15, quantity: 10_000 },
+          { operation: 'sell', 'unit-cost': 25, quantity: 5_000 }
+        ].to_json
+      end
+
+      let(:expected_response) do
+        [{ tax: 0 }, { tax: 0 }, { tax: 0 }, { tax: 10_000.0 }].to_json
+      end
+
+      it 'returns the first tax as zero and the second as 10_000' do
+        expect(subject).to  eq(expected_response)
+      end
+    end
+
+    context 'with Sixth input exemple' do
+      let(:input) do
+        [
+          { operation: 'buy', 'unit-cost': 10, quantity: 10_000 },
+          { operation: 'sell', 'unit-cost': 2, quantity: 5_000 },
+          { operation: 'sell', 'unit-cost': 20, quantity: 2_000 },
+          { operation: 'sell', 'unit-cost': 20, quantity: 2_000 },
+          { operation: 'sell', 'unit-cost': 25, quantity: 1_000 }
+        ].to_json
+      end
+
+      let(:expected_response) do
+        [{ tax: 0 }, { tax: 0 }, { tax: 0 }, { tax: 0 }, { tax: 3_000.0 }].to_json
+      end
+
+      it 'returns only the last tax as 3_000 and the rest zero' do
+        expect(subject).to eq(expected_response)
+      end
     end
   end
 
-  context 'with Second input exemple' do
-    let(:input) do
-      [
-        { operation: 'buy', 'unit-cost': 10, quantity: 10_000 },
-        { operation: 'sell', 'unit-cost': 20, quantity: 5_000 },
-        { operation: 'sell', 'unit-cost': 5, quantity: 5_000 }
-      ].to_json
+  context 'when input is invalid' do
+    context 'when  input is nil' do
+      let(:input) { nil }
+
+      it 'returns error message' do
+        expect(subject).to eq('Operations cannot be null or a empty array')
+      end
     end
 
-    let(:expected_response) do
-      [{ tax: 0 }, { tax: 10_000.0 }, { tax: 0 }].to_json
+    context 'when  input is empty line' do
+      let(:input) { '' }
+
+      it 'returns error message' do
+        expect(subject).to eq('Operations cannot be null or a empty array')
+      end
     end
 
-    it 'returns the first tax as 10_000 and the secon zero' do
-      expect(subject).to  eq(expected_response)
-    end
-  end
+    context 'when input is an empty array' do
+      let(:input) { '[]' }
 
-  context 'with Third input exemple' do
-    let(:input) do
-      [
-        { operation: 'buy', 'unit-cost': 10, quantity: 10_000 },
-        { operation: 'sell', 'unit-cost': 5, quantity: 5_000 },
-        { operation: 'sell', 'unit-cost': 20, quantity: 5_000 }
-      ].to_json
+      it 'returns error message' do
+        expect(subject).to eq('Operations cannot be null or a empty array')
+      end
     end
 
-    let(:expected_response) do
-      [{ tax: 0 }, { tax: 0 }, { tax: 5_000.0 }].to_json
-    end
+    context 'when input is an invalid json' do
+      let(:input) { '["invalid": 1]' }
 
-    it 'returns the first tax as zero and the second as 5_000' do
-      expect(subject).to  eq(expected_response)
-    end
-  end
-
-  context 'with Fourth input exemple' do
-    let(:input) do
-      [
-        { operation: 'buy', 'unit-cost': 10, quantity: 10_000 },
-        { operation: 'buy', 'unit-cost': 25, quantity: 5_000 },
-        { operation: 'sell', 'unit-cost': 15, quantity: 10_000 }
-      ].to_json
-    end
-
-    let(:expected_response) do
-      [{ tax: 0 }, { tax: 0 }, { tax: 0 }].to_json
-    end
-
-    it 'returns the only sell tax as zero' do
-      expect(subject).to  eq(expected_response)
-    end
-  end
-
-  context 'with Fifth input exemple' do
-    let(:input) do
-      [
-        { operation: 'buy', 'unit-cost': 10, quantity: 10_000 },
-        { operation: 'buy', 'unit-cost': 25, quantity: 5_000 },
-        { operation: 'sell', 'unit-cost': 15, quantity: 10_000 },
-        { operation: 'sell', 'unit-cost': 25, quantity: 5_000 }
-      ].to_json
-    end
-
-    let(:expected_response) do
-      [{ tax: 0 }, { tax: 0 }, { tax: 0 }, { tax: 10_000.0 }].to_json
-    end
-
-    it 'returns the first tax as zero and the second as 10_000' do
-      expect(subject).to  eq(expected_response)
-    end
-  end
-
-  context 'with Sixth input exemple' do
-    let(:input) do
-      [
-        { operation: 'buy', 'unit-cost': 10, quantity: 10_000 },
-        { operation: 'sell', 'unit-cost': 2, quantity: 5_000 },
-        { operation: 'sell', 'unit-cost': 20, quantity: 2_000 },
-        { operation: 'sell', 'unit-cost': 20, quantity: 2_000 },
-        { operation: 'sell', 'unit-cost': 25, quantity: 1_000 }
-      ].to_json
-    end
-
-    let(:expected_response) do
-      [{ tax: 0 }, { tax: 0 }, { tax: 0 }, { tax: 0 }, { tax: 3_000.0 }].to_json
-    end
-
-    it 'returns only the last tax as 3_000 and the rest zero' do
-      expect(subject).to eq(expected_response)
+      it 'returns error message' do
+        expect(subject).to eq('Invalid json string')
+      end
     end
   end
 end
