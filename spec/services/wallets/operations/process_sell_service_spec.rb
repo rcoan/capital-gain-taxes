0479ea305aafa47c977c08e366de387b97685a00
rcoan <rvc.coan@gmail.com>
2021-10-23 10:42:24 -0300

    add tests to process sell service

--- spec/services/wallets/operations/process_sell_service_spec.rb
+++ spec/services/wallets/operations/process_sell_service_spec.rb
@@ -4,5 +4,150 @@ require 'spec_helper'
 
 RSpec.describe Wallets::Operations::ProcessSellService do
   describe '#call' do
+    subject { described_class.call(input) }
+
+    before do
+      allow(Wallets::Operations::CalculateOperationTax).to receive(:call).and_return(tax)
+    end
+
+    context 'when previous operations had no loss' do
+      context 'when sell has profit' do
+        let(:tax) { 10 }
+        let(:input) do
+          {
+            unit_cost: 10,
+            quantity:10,
+            total_stocks:10,
+            weighted_average_cost:5,
+            total_profit: 0
+          }
+        end
+
+        it 'returns total profit of current operation' do
+          expected_result = {
+            operation_tax: tax,
+            total_stocks: 0,
+            total_profit: 50
+          }
+
+          expect(subject).to eq(expected_result)
+        end
+      end
+
+      context 'when sell has loss' do
+        let(:tax) { 0 }
+        let(:input) do
+          {
+            unit_cost: 10,
+            quantity:10,
+            total_stocks:10,
+            weighted_average_cost:15,
+            total_profit: 100
+          }
+        end
+
+        it 'returns total profit less then it was before' do
+          expected_result = {
+            operation_tax: tax,
+            total_stocks: 0,
+            total_profit: -50
+          }
+
+          expect(subject).to eq(expected_result)
+        end
+      end
+
+      context 'when sell has no loss or profit' do
+        let(:tax) { 0 }
+        let(:input) do
+          {
+            unit_cost: 10,
+            quantity:10,
+            total_stocks:10,
+            weighted_average_cost:10,
+            total_profit: 1000
+          }
+        end
+
+        it 'returns total profit zeroed' do
+          expected_result = {
+            operation_tax: tax,
+            total_stocks: 0,
+            total_profit: 0
+          }
+
+          expect(subject).to eq(expected_result)
+        end
+      end
+    end
+
+    context 'when previous operations had loss' do
+      let(:tax) { 0 }
+      context 'when sell has profit' do
+        let(:input) do
+          {
+            unit_cost: 10,
+            quantity:10,
+            total_stocks:10,
+            weighted_average_cost:5,
+            total_profit: -100
+          }
+        end
+
+        it 'returns total profit of current operation' do
+          expected_result = {
+            operation_tax: tax,
+            total_stocks: 0,
+            total_profit: -50
+          }
+
+          expect(subject).to eq(expected_result)
+        end
+      end
+
+      context 'when sell has loss' do
+        let(:input) do
+          {
+            unit_cost: 10,
+            quantity:10,
+            total_stocks:10,
+            weighted_average_cost:15,
+            total_profit: -100
+          }
+        end
+
+        it 'returns total profit less then it was before' do
+          expected_result = {
+            operation_tax: tax,
+            total_stocks: 0,
+            total_profit: -150
+          }
+
+          expect(subject).to eq(expected_result)
+        end
+      end
+
+      context 'when sell has no loss or profit' do
+        let(:input) do
+          {
+            unit_cost: 10,
+            quantity:10,
+            total_stocks:10,
+            weighted_average_cost:10,
+            total_profit: -100
+          }
+        end
+
+        it 'returns total profit equal as before' do
+          expected_result = {
+            operation_tax: tax,
+            total_stocks: 0,
+            total_profit: -100
+          }
+
+          expect(subject).to eq(expected_result)
+        end
+      end
+    end
   end
 end
