441e56a1c654b220885d9959045c0f123333b0b5
rcoan <rvc.coan@gmail.com>
2021-10-23 11:55:56 -0300

    add tests to calculate operation tax service

--- spec/services/wallets/operations/calculate_operation_tax_spec.rb
+++ spec/services/wallets/operations/calculate_operation_tax_spec.rb
@@ -16,21 +16,57 @@ RSpec.describe Wallets::Operations::CalculateOperationTax do
       }
     end
 
-    context 'when operation buy' do
+    context 'when operation sell' do
       context 'when Total above 20_000' do
         context 'when unit cost above average cost' do
           context 'when profit is positive' do
+            let(:operation) { 'sell' }
+            let(:total_value) { 20_001 }
+            let(:weighted_average_cost) { 15 }
+            let(:unit_cost) { 25 }
+            let(:profit) { 1000 }
+
+            it 'returns the tax as 20% of profit' do
+              expect(subject).to eq(200)
+            end
           end
 
           context 'when profit is NOT positive' do
+            let(:operation) { 'sell' }
+            let(:total_value) { 20_001 }
+            let(:weighted_average_cost) { 15 }
+            let(:unit_cost) { 25 }
+            let(:profit) { -100 }
+
+            it 'returns the tax as 0 since there was loss' do
+              expect(subject).to eq(0)
+            end
           end
         end
 
         context 'when unit cost NOT above average cost' do
           context 'when profit is positive' do
+            let(:operation) { 'sell' }
+            let(:total_value) { 20_001 }
+            let(:weighted_average_cost) { 15 }
+            let(:unit_cost) { 10 }
+            let(:profit) { 1000 }
+
+            it 'returns the tax as 0 since the sell value was below average' do
+              expect(subject).to eq(0)
+            end
           end
 
           context 'when profit is NOT positive' do
+            let(:operation) { 'sell' }
+            let(:total_value) { 20_001 }
+            let(:weighted_average_cost) { 15 }
+            let(:unit_cost) { 10 }
+            let(:profit) { -100 }
+
+            it 'returns the tax as 0 since the sell value was below average and there was loss' do
+              expect(subject).to eq(0)
+            end
           end
         end
       end
@@ -38,37 +74,112 @@ RSpec.describe Wallets::Operations::CalculateOperationTax do
       context 'when Total NOT above 20_000' do
         context 'when unit cost above average cost' do
           context 'when profit is positive' do
+            let(:operation) { 'sell' }
+            let(:total_value) { 19_999 }
+            let(:weighted_average_cost) { 15 }
+            let(:unit_cost) { 25 }
+            let(:profit) { 1000 }
+
+            it 'returns the tax as 0 since the total value was not above 20_000' do
+              expect(subject).to eq(0)
+            end
           end
 
           context 'when profit is NOT positive' do
+            let(:operation) { 'sell' }
+            let(:total_value) { 19_999 }
+            let(:weighted_average_cost) { 15 }
+            let(:unit_cost) { 25 }
+            let(:profit) { -100 }
+
+            it 'returns the tax as 0 since the total value was not above 20_000 and \
+              there was loss' do
+              expect(subject).to eq(0)
+            end
           end
         end
 
         context 'when unit cost NOT above average cost' do
           context 'when profit is positive' do
+            let(:operation) { 'sell' }
+            let(:total_value) { 19_999 }
+            let(:weighted_average_cost) { 15 }
+            let(:unit_cost) { 10 }
+            let(:profit) { 1000 }
+
+            it 'returns the tax as 0 since the total value was not above 20_000 and \
+              cost was below average cost' do
+              expect(subject).to eq(0)
+            end
           end
 
           context 'when profit is NOT positive' do
+            let(:operation) { 'sell' }
+            let(:total_value) { 19_999 }
+            let(:weighted_average_cost) { 15 }
+            let(:unit_cost) { 10 }
+            let(:profit) { -100 }
+
+            it 'returns the tax as 0 since the total value was not above 20_000 and \
+              cost was below average cost and there was loss' do
+              expect(subject).to eq(0)
+            end
           end
         end
       end
     end
 
-    context 'when operation sell' do
+    context 'when operation buy' do
       context 'when Total above 20_000' do
         context 'when unit cost above average cost' do
           context 'when profit is positive' do
+            let(:operation) { 'buy' }
+            let(:total_value) { 20_001 }
+            let(:weighted_average_cost) { 15 }
+            let(:unit_cost) { 25 }
+            let(:profit) { 1000 }
+
+            it 'returns the tax as zero' do
+              expect(subject).to eq(0)
+            end
           end
 
           context 'when profit is NOT positive' do
+            let(:operation) { 'buy' }
+            let(:total_value) { 20_001 }
+            let(:weighted_average_cost) { 15 }
+            let(:unit_cost) { 25 }
+            let(:profit) { -100 }
+
+            it 'returns the tax as zero' do
+              expect(subject).to eq(0)
+            end
           end
         end
 
         context 'when unit cost NOT above average cost' do
           context 'when profit is positive' do
+            let(:operation) { 'buy' }
+            let(:total_value) { 20_001 }
+            let(:weighted_average_cost) { 15 }
+            let(:unit_cost) { 10 }
+            let(:profit) { 1000 }
+
+            it 'returns the tax as zero' do
+              expect(subject).to eq(0)
+            end
           end
 
           context 'when profit is NOT positive' do
+            let(:operation) { 'buy' }
+            let(:total_value) { 20_001 }
+            let(:weighted_average_cost) { 15 }
+            let(:unit_cost) { 10 }
+            let(:profit) { -100 }
+
+            it 'returns the tax as zero' do
+              expect(subject).to eq(0)
+            end
           end
         end
       end
@@ -76,17 +187,53 @@ RSpec.describe Wallets::Operations::CalculateOperationTax do
       context 'when Total NOT above 20_000' do
         context 'when unit cost above average cost' do
           context 'when profit is positive' do
+            let(:operation) { 'buy' }
+            let(:total_value) { 19_999 }
+            let(:weighted_average_cost) { 15 }
+            let(:unit_cost) { 25 }
+            let(:profit) { 1000 }
+
+            it 'returns the tax as zero' do
+              expect(subject).to eq(0)
+            end
           end
 
           context 'when profit is NOT positive' do
+            let(:operation) { 'buy' }
+            let(:total_value) { 19_999 }
+            let(:weighted_average_cost) { 15 }
+            let(:unit_cost) { 25 }
+            let(:profit) { -100 }
+
+            it 'returns the tax as zero' do
+              expect(subject).to eq(0)
+            end
           end
         end
 
         context 'when unit cost NOT above average cost' do
           context 'when profit is positive' do
+            let(:operation) { 'buy' }
+            let(:total_value) { 19_999 }
+            let(:weighted_average_cost) { 15 }
+            let(:unit_cost) { 10 }
+            let(:profit) { 1000 }
+
+            it 'returns the tax as zero' do
+              expect(subject).to eq(0)
+            end
           end
 
           context 'when profit is NOT positive' do
+            let(:operation) { 'buy' }
+            let(:total_value) { 19_999 }
+            let(:weighted_average_cost) { 15 }
+            let(:unit_cost) { 10 }
+            let(:profit) { -100 }
+
+            it 'returns the tax as zero' do
+              expect(subject).to eq(0)
+            end
           end
         end
       end
