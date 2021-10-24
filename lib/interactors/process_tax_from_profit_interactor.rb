3e393e1bc23d7a320253367e62fd29fbcfac212b
rcoan <rvc.coan@gmail.com>
2021-10-24 15:04:53 -0300

    refactor interactor to use service to build operations

--- lib/interactors/process_tax_from_profit_interactor.rb
+++ lib/interactors/process_tax_from_profit_interactor.rb
@@ -6,31 +6,16 @@ class ProcessTaxFromProfitInteractor
   end
 
   def call(operations_json)
-    parsed_operations = parse_operations(operations_json)
-    wallet = Wallet.new(operations: parsed_operations)
+    operations_list = Wallets::Operations::BuildOperationsFromJsonService.call(operations_json)
+
+    wallet = Wallet.new(operations: operations_list)
+
+    return wallet.errors if wallet.invalid?
 
     wallet = Wallets::ProcessOperationsTaxService.call(wallet)
 
     WalletPresenter.new(wallet).json_formatted_taxes
-  rescue JSON::ParserError => e
-    'Invalid json string'
   rescue StandardError => e
     e.message
-  end
-
-  private
-
-  def parse_operations(operations_json)
-    raise_invalid_input_error if operations_json.nil? || operations_json.empty?
-
-    parsed_operations = JSON.parse(operations_json)
-
-    raise_invalid_input_error if parsed_operations.nil? || parsed_operations.empty?
-
-    parsed_operations
-  end
-
-  def raise_invalid_input_error
-    raise StandardError, 'Operations cannot be null or a empty array'
   end
 end
